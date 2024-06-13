f07f55a2dc41b687e83ff27ba01d67ff1bab0efc
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 17:07:45 +0200

    uncomment llama3 part temporary

--- src/function_app.py
+++ src/function_app.py
@@ -1,39 +1,39 @@
 import azure.functions as func
 import logging
-from openai import OpenAI
+#from openai import OpenAI
 from os import getenv
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
 
-load_dotenv()
+# load_dotenv()
 
-app = func.FunctionApp()
-client = OpenAI(
-  api_key=getenv('DATABRICKS_TOKEN'),
-  base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
-)
+# app = func.FunctionApp()
+# client = OpenAI(
+#   api_key=getenv('DATABRICKS_TOKEN'),
+#   base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
+# )
 
 @app.function_name(name="generate")
 @app.route(route="generate", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info('Python HTTP trigger function processed a request.')
-    chat_completion = client.chat.completions.create(
-    messages=[
-    {
-        "role": "system",
-        "content": "You are an AI assistant"
-    },
-    {
-        "role": "user",
-        "content": "Tell me about Large Language Models"
-    }
-    ],
-    model="databricks-meta-llama-3-70b-instruct",
-    max_tokens=256
-    )
-    logger.info(chat_completion)
+    # chat_completion = client.chat.completions.create(
+    # messages=[
+    # {
+    #     "role": "system",
+    #     "content": "You are an AI assistant"
+    # },
+    # {
+    #     "role": "user",
+    #     "content": "Tell me about Large Language Models"
+    # }
+    # ],
+    # model="databricks-meta-llama-3-70b-instruct",
+    # max_tokens=256
+    # )
+    # logger.info(chat_completion)
     return func.HttpResponse(
-       chat_completcompletion.choices[0].message.contention,
+       "hey what up",
         status_code=200
         )
