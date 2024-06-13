c3f04ddd5d02528caec6f6f30fb0f08e1035e2f5
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 19:06:09 +0200

    uncomment llama3 call code

--- src/function_app.py
+++ src/function_app.py
@@ -4,7 +4,6 @@ from os import getenv
 
 from openai import OpenAI
 
-import dotenv
 
 logger = logging.getLogger(__name__)
 
@@ -18,23 +17,21 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info(f"Python HTTP trigger function processed a request: {req}")
     logger.info("DATABRICKS_TOKEN: %s", getenv("DATABRICKS_TOKEN"))
     try:
-        # client = OpenAI(
-        #     api_key=getenv("DATABRICKS_TOKEN"),
-        #     base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
-        # )
-        # chat_completion = client.chat.completions.create(
-        #     messages=[
-        #         {"role": "system", "content": "You are an AI assistant"},
-        #         {"role": "user", "content": "Tell me about Large Language Models"},
-        #     ],
-        #     model="databricks-meta-llama-3-70b-instruct",
-        #     max_tokens=256,
-        # )
-        # logger.info(chat_completion)
-        return_value = "Mocked response from llama3"
-        return func.HttpResponse(
-            return_value, status_code=200
+        client = OpenAI(
+            api_key=getenv("DATABRICKS_TOKEN"),
+            base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
         )
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": "You are an AI assistant"},
+                {"role": "user", "content": "Tell me about Large Language Models"},
+            ],
+            model="databricks-meta-llama-3-70b-instruct",
+            max_tokens=256,
+        )
+        logger.info(chat_completion)
+        return_value = "Mocked response from llama3"
+        return func.HttpResponse(return_value, status_code=200)
     except Exception as e:
         logger.error(e)
         return func.HttpResponse("Internal Server Error", status_code=500)
