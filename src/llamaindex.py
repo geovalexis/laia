1e9389e8aa9d69e511e0927127ffb7298503f12b
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-14 01:16:56 +0200

    create chat engine with llamaindex

--- src/llamaindex.py
+++ src/llamaindex.py
@@ -1,13 +1,19 @@
+import pathlib
+from os import getenv
+
+from dotenv import load_dotenv
 from llama_index.core import (
+    ChatPromptTemplate,
     KeywordTableIndex,
+    PromptTemplate,
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
 )
-import pathlib
+from llama_index.core.chat_engine.types import ChatMode
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.llms.databricks import Databricks
-from os import getenv
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -35,8 +41,36 @@ else:
     # Save the index for later use
     index.storage_context.persist(INDEX_PERSIST_DIR)
 
+# Define a custom prompt
+# message_templates = [
+#     ChatMessage(
+#         content="You are an expert doctor with full experience assessing the specialism for a case given a set of symptoms given by a patience. You should always respond with only ONE WORD!",
+#         role=MessageRole.SYSTEM,
+#     ),
+#     ChatMessage(
+#         content="Redirect me to the right specialism based on the following symptoms: {symptoms}",
+#         role=MessageRole.USER,
+#     ),
+# ]
+# chat_template = ChatPromptTemplate.from_messages(message_templates)
 
-# get response from query
-query_engine = index.as_query_engine(llm=llm)
-response = query_engine.query("I have a leg inflamed, what specialty should I go?")
+# # you can create message prompts (for chat API)
+# messages = chat_template.format_messages(llm=llm, symptoms="specialty")
+
+# # # or easily convert to text prompt (for completion API)
+# prompt = chat_template.format(llm=llm, symptoms="specialty")
+
+
+chat_engine = index.as_chat_engine(
+    llm=llm,
+    memory=ChatMemoryBuffer.from_defaults(token_limit=10500),
+    chat_mode=ChatMode.CONTEXT,
+    system_prompt="You are an expert doctor with full experience assessing the specialism for a case given a set of symptoms given by a patience. You should always respond with only ONE specialist!",
+)
+response = chat_engine.chat(
+    "I have a leg inflamed, what specialty should I go?", chat_history=[]
+)
+# chat_engine.reset()
+# print(chat_engine.chat_history)
+# response = chat_engine.chat("I have a leg inflamed, what specialty should I go?")
 print(response)
