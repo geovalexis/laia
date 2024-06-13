8cb5f44a43a3460372ebe54bfe1b77f811594252
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 18:49:11 +0200

    mock response from llama3

--- src/function_app.py
+++ src/function_app.py
@@ -16,21 +16,22 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info(f"Python HTTP trigger function processed a request: {req}")
     logger.info("DATABRICKS_TOKEN: %s", getenv("DATABRICKS_TOKEN"))
     try:
-        client = OpenAI(
-            api_key=getenv("DATABRICKS_TOKEN"),
-            base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
-        )
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {"role": "system", "content": "You are an AI assistant"},
-                {"role": "user", "content": "Tell me about Large Language Models"},
-            ],
-            model="databricks-meta-llama-3-70b-instruct",
-            max_tokens=256,
-        )
-        logger.info(chat_completion)
+        # client = OpenAI(
+        #     api_key=getenv("DATABRICKS_TOKEN"),
+        #     base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
+        # )
+        # chat_completion = client.chat.completions.create(
+        #     messages=[
+        #         {"role": "system", "content": "You are an AI assistant"},
+        #         {"role": "user", "content": "Tell me about Large Language Models"},
+        #     ],
+        #     model="databricks-meta-llama-3-70b-instruct",
+        #     max_tokens=256,
+        # )
+        # logger.info(chat_completion)
+        return_value = "Mocked response from llama3"
         return func.HttpResponse(
-            chat_completion.choices[0].message.content, status_code=200
+            return_value, status_code=200
         )
     except Exception as e:
         logger.error(e)
