14843dded247dd3006af2f9e0d522797506cf9eb
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 17:39:31 +0200

    fix typo

--- src/function_app.py
+++ src/function_app.py
@@ -9,15 +9,15 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 app = func.FunctionApp()
-client = OpenAI(
-  api_key=getenv('DATABRICKS_TOKEN'),
-  base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
-)
 
 @app.function_name(name="generate")
 @app.route(route="generate", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info('Python HTTP trigger function processed a request.')
+    client = OpenAI(
+        api_key=getenv('DATABRICKS_TOKEN'),
+        base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
+    )
     chat_completion = client.chat.completions.create(
     messages=[
     {
@@ -34,6 +34,6 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     )
     logger.info(chat_completion)
     return func.HttpResponse(
-       chat_completcompletion.choices[0].message.contention,
+       chat_completion.choices[0].message.contention,
         status_code=200
         )
