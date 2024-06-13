abf41b779af0b98ce91df9c0f3fde463e2c6178d
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-14 01:27:22 +0200

    add chat history and system prompt dynamically

--- src/llama3.py
+++ src/llama3.py
@@ -1,32 +1,27 @@
-from openai import OpenAI
 from os import getenv
+
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
 # How to get your Databricks token: https://docs.databricks.com/en/dev-tools/auth/pat.html
-DATABRICKS_TOKEN = getenv('DATABRICKS_TOKEN')
+DATABRICKS_TOKEN = getenv("DATABRICKS_TOKEN")
 # Alternatively in a Databricks notebook you can use this:
 # DATABRICKS_TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
 
 client = OpenAI(
-  api_key=DATABRICKS_TOKEN,
-  base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
+    api_key=DATABRICKS_TOKEN,
+    base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
 )
 
 chat_completion = client.chat.completions.create(
-  messages=[
-  {
-    "role": "system",
-    "content": "You are an AI assistant"
-  },
-  {
-    "role": "user",
-    "content": "Tell me about Large Language Models"
-  }
-  ],
-  model="databricks-meta-llama-3-70b-instruct",
-  max_tokens=256
+    messages=[
+        {"role": "system", "content": "You are an AI assistant"},
+        {"role": "user", "content": "Tell me about Large Language Models"},
+    ],
+    model="databricks-meta-llama-3-70b-instruct",
+    max_tokens=256,
 )
 
 print(chat_completion.choices[0].message.content)
--- src/llamaindex.py
+++ src/llamaindex.py
@@ -3,9 +3,7 @@ from os import getenv
 
 from dotenv import load_dotenv
 from llama_index.core import (
-    ChatPromptTemplate,
     KeywordTableIndex,
-    PromptTemplate,
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
@@ -17,8 +15,10 @@ from llama_index.llms.databricks import Databricks
 
 load_dotenv()
 
+# Define static data paths
 INDEX_PERSIST_DIR = "./index"
 DOCS_DATA_DIR = "./data"
+PROMPTS_DIR = "./prompts"
 
 # How to get your Databricks token: https://docs.databricks.com/en/dev-tools/auth/pat.html
 DATABRICKS_TOKEN = getenv("DATABRICKS_TOKEN")
@@ -41,36 +41,28 @@ else:
     # Save the index for later use
     index.storage_context.persist(INDEX_PERSIST_DIR)
 
-# Define a custom prompt
-# message_templates = [
-#     ChatMessage(
-#         content="You are an expert doctor with full experience assessing the specialism for a case given a set of symptoms given by a patience. You should always respond with only ONE WORD!",
-#         role=MessageRole.SYSTEM,
-#     ),
-#     ChatMessage(
-#         content="Redirect me to the right specialism based on the following symptoms: {symptoms}",
-#         role=MessageRole.USER,
-#     ),
-# ]
-# chat_template = ChatPromptTemplate.from_messages(message_templates)
+# Read system prompt
+with open(pathlib.Path(PROMPTS_DIR) / "system.txt") as f:
+    system_prompt = f.read()
 
-# # you can create message prompts (for chat API)
-# messages = chat_template.format_messages(llm=llm, symptoms="specialty")
-
-# # # or easily convert to text prompt (for completion API)
-# prompt = chat_template.format(llm=llm, symptoms="specialty")
-
-
+# Create chat engine
 chat_engine = index.as_chat_engine(
     llm=llm,
     memory=ChatMemoryBuffer.from_defaults(token_limit=10500),
     chat_mode=ChatMode.CONTEXT,
-    system_prompt="You are an expert doctor with full experience assessing the specialism for a case given a set of symptoms given by a patience. You should always respond with only ONE specialist!",
+    system_prompt=system_prompt,
 )
+chat_history = [
+    ChatMessage(
+        role=MessageRole.USER,
+        content="I have a leg inflamed, what specialty should I go?",
+    ),
+    ChatMessage(
+        role=MessageRole.ASSISTANT,
+        content="Based on your symptoms, I would recommend that you see a Dermatologist.",
+    ),
+]
 response = chat_engine.chat(
-    "I have a leg inflamed, what specialty should I go?", chat_history=[]
+    "I have a leg inflamed, what specialty should I go?", chat_history=chat_history
 )
-# chat_engine.reset()
-# print(chat_engine.chat_history)
-# response = chat_engine.chat("I have a leg inflamed, what specialty should I go?")
 print(response)
