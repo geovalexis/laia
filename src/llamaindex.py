a3c5f4f630cafaf1f963d5dac62549042d4d6fca
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 20:57:43 +0200

    use llama3 model and add context data

--- src/llamaindex.py
+++ src/llamaindex.py
@@ -12,7 +12,7 @@ documents = SimpleDirectoryReader("data").load_data()
 
 # define LLM
 llm = Databricks(
-    model="databricks-dbrx-instruct",
+    model="databricks-meta-llama-3-70b-instruct",
     api_key=DATABRICKS_TOKEN,
     api_base="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
     max_tokens=256,
@@ -22,5 +22,5 @@ index = KeywordTableIndex.from_documents(documents, llm=llm)
 
 # get response from query
 query_engine = index.as_query_engine(llm=llm)
-response = query_engine.query("Hola, quien soy?")
+response = query_engine.query("I have a nose congestion, what specialty should I go?")
 print(response)
