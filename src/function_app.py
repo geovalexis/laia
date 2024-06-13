9277e9a291b4c7080aa43434653431fbf9dde116
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 16:54:18 +0200

    integrate llama3 call into function app

--- src/function_app.py
+++ src/function_app.py
@@ -1,13 +1,39 @@
 import azure.functions as func
 import logging
+from openai import OpenAI
+from os import getenv
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
 
 app = func.FunctionApp()
+client = OpenAI(
+  api_key=getenv('DATABRICKS_TOKEN'),
+  base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
+)
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+@app.function_name(name="generate")
+@app.route(route="generate", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logger.info('Python HTTP trigger function processed a request.')
+    chat_completion = client.chat.completions.create(
+    messages=[
+    {
+        "role": "system",
+        "content": "You are an AI assistant"
+    },
+    {
+        "role": "user",
+        "content": "Tell me about Large Language Models"
+    }
+    ],
+    model="databricks-meta-llama-3-70b-instruct",
+    max_tokens=256
+    )
+    logger.info(chat_completion)
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully.",
+       chat_completcompletion.choices[0].message.contention,
         status_code=200
         )
