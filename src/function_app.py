bd2a09712ddcb791764622ec6ce9345564f67ff0
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-13 17:45:33 +0200

    fix typo function app

--- src/function_app.py
+++ src/function_app.py
@@ -10,30 +10,24 @@ load_dotenv()
 
 app = func.FunctionApp()
 
+
 @app.function_name(name="generate")
 @app.route(route="generate", auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info('Python HTTP trigger function processed a request.')
+    logger.info("Python HTTP trigger function processed a request.")
     client = OpenAI(
-        api_key=getenv('DATABRICKS_TOKEN'),
-        base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints"
+        api_key=getenv("DATABRICKS_TOKEN"),
+        base_url="https://adb-2978037251816793.13.azuredatabricks.net/serving-endpoints",
     )
     chat_completion = client.chat.completions.create(
-    messages=[
-    {
-        "role": "system",
-        "content": "You are an AI assistant"
-    },
-    {
-        "role": "user",
-        "content": "Tell me about Large Language Models"
-    }
-    ],
-    model="databricks-meta-llama-3-70b-instruct",
-    max_tokens=256
+        messages=[
+            {"role": "system", "content": "You are an AI assistant"},
+            {"role": "user", "content": "Tell me about Large Language Models"},
+        ],
+        model="databricks-meta-llama-3-70b-instruct",
+        max_tokens=256,
     )
     logger.info(chat_completion)
     return func.HttpResponse(
-       chat_completion.choices[0].message.contention,
-        status_code=200
-        )
+        chat_completion.choices[0].message.content, status_code=200
+    )
