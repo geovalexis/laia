e79370a02d95575b8f32bd5e1c759dd4becf4c9a
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-14 15:51:40 +0200

    add text2audio engine

---
+++ laia/text2audio.py
@@ -0,0 +1,32 @@
+from os import getenv
+from pathlib import Path
+
+from dotenv import load_dotenv
+from elevenlabs import save
+from elevenlabs.client import ElevenLabs
+
+load_dotenv()
+
+
+client = ElevenLabs(
+    api_key=getenv("ELEVENLABS_API_KEY"),
+)
+
+
+def generate_audio(
+    text: str, voice: str = "Glinda", model: str = "eleven_multilingual_v1"
+) -> Path:
+    audio = client.generate(
+        text=text,
+        voice=voice,
+        model=model,
+    )
+    audio_path = Path(f"audio.mp3")
+    save(audio, str(audio_path))
+    return audio_path
+
+
+if __name__ == "__main__":
+    text = "Hello, how are you?"
+    audio_path = generate_audio(text)
+    print(f"Audio saved as '{audio_path}'")
