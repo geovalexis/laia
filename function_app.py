bae6bf7e0f3382fc3e666842c3f85fce90bb3e3b
Geovanny Risco <geovanny.risco@teladochealth.com>
2024-06-14 16:19:31 +0200

    add new endpoint for avatar

--- function_app.py
+++ function_app.py
@@ -3,7 +3,9 @@ import logging
 import azure.functions as func
 from llama_index.core.llms import ChatMessage
 
+from laia.avatar import generate_video
 from laia.rag import generate_response, hydrate_conversation
+from laia.text2audio import generate_audio
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +15,7 @@ app = func.FunctionApp()
 
 @app.function_name(name="generate")
 @app.route(route="generate", auth_level=func.AuthLevel.ANONYMOUS)
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
+def generate_conversation(req: func.HttpRequest) -> func.HttpResponse:
     logger.info(f"Python HTTP trigger function processed a request: {req}")
     req_body = req.get_json()
     logger.info(f"Request body: {req_body}")
@@ -25,6 +27,25 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         hydrate_conversation(messages, member_id=int(req_body["memberId"]))
         response = generate_response(messages)
         return func.HttpResponse(str(response), status_code=200)
+    except Exception as e:
+        logger.error(e)
+        return func.HttpResponse("Internal Server Error", status_code=500)
+
+
+@app.function_name(name="avatar")
+@app.route(route="avatar", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
+def generate_avatar_video(req: func.HttpRequest) -> func.HttpResponse:
+    logger.info(f"Python HTTP trigger function processed a request: {req}")
+    req_body = req.get_json()
+    logger.info(f"Request body: {req_body}")
+    try:
+        logger.info("Generating audio...")
+        audio_path = generate_audio(text=req_body["message"])
+        logger.info("Generating video...")
+        video_path = generate_video(audio_path)
+        return func.HttpResponse(
+            body=video_path.read_bytes(), status_code=200, mimetype="video/mp4"
+        )
     except Exception as e:
         logger.error(e)
         return func.HttpResponse("Internal Server Error", status_code=500)
--- laia/avatar.py
+++ laia/avatar.py
@@ -1,39 +1,41 @@
+import logging
 import os
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
 
-def upload_audio(audio_path):
-    if not os.path.isfile(audio_path):
-        print(f"File not found: {audio_path}")
-        return
 
+def generate_video(audio_path):
     # Create form data
     with open(audio_path, "rb") as file:
         form_data = {"audio": file}
-        headers = {"X-API-KEY": os.getenv("JQBROTON_API_KEY")}
 
         try:
             # Send POST request
             response = requests.post(
                 "https://b0fd-139-47-125-37.ngrok-free.app/generate",
                 files=form_data,
-                headers=headers,
+                headers={"X-API-KEY": os.getenv("JQBROTON_API_KEY")},
             )
             response.raise_for_status()
 
             # Save the response blob as a file
-            result_file = "result_video.mp4"
+            result_file = Path("result_video.mp4")
             with open(result_file, "wb") as video_file:
                 video_file.write(response.content)
-            print(f"File uploaded successfully. Video saved as '{result_file}'")
+            logger.info(f"File uploaded successfully. Video saved as '{result_file}'")
+
+            return result_file
 
         except requests.exceptions.RequestException as e:
-            print(f"Error uploading file: {e}")
+            logger.error(f"Error uploading file: {e}")
+            raise
 
 
 if __name__ == "__main__":
-    upload_audio("audio.mp3")
+    generate_video("audio.mp3")
